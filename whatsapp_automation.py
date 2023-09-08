9a9262217a4a815cfe8a501b60e65cdc42654977
Aslin Joel R <aslinjoelr@outlook.com>
2023-09-08 15:07:20 +0530

    Add files via upload

---
+++ whatsapp_automation.py
@@ -0,0 +1,15 @@
+import pyautogui as pg
+import time
+
+print("Starting in 10 seconds")
+time.sleep(15)
+
+for i in range(1000):
+	
+	pg.write("..... Theppa pea anga .....\n")
+	
+	pg.press("Enter")
+
+
+	
+
